394203cf0545be8d461d045a830bc879d38552f4
anggaanugrah <angga.anugrah.aa@gmail.com>
2023-02-16 22:53:39 +0800

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color Style
+Color whiteColor = const Color(0xffFFFFFF);
+Color whiteAltColor = const Color(0xffFAFAFF);
+Color greyColor = const Color(0xffB0B0B0);
+Color blackColor = const Color(0xff30343F);
+Color redColor = const Color(0xffEE2B47);
+Color greenColor = const Color(0xff00C897);
+Color linkColor = const Color(0xff53C1F9);
+Color lightBackground = const Color(0xffF8F8F8);
+Color darkBackground = const Color(0xff30343F);
+
+// Space Mono Text Style
+TextStyle numberBlackTextStyle = GoogleFonts.spaceMono(
+  color: blackColor,
+);
+
+TextStyle numberWhiteTextStyle = GoogleFonts.spaceMono(
+  color: whiteColor,
+);
+
+// Poppins Text Style
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+TextStyle linkTextStyle = GoogleFonts.poppins(
+  color: linkColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+
+// Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
